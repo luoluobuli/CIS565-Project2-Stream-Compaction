fa821d7a62220d4da0095d2fa3921703c7fe8842
luoluobuli <joranlicn@gmail.com>
2025-09-17 10:54:14 -0400

    finish naive scan

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include "cpu.h"
+#include <iostream>
 
 #include "common.h"
 
@@ -17,9 +18,45 @@ namespace StreamCompaction {
          * For performance analysis, this is supposed to be a simple for loop.
          * (Optional) For better understanding before starting moving to GPU, you can simulate your GPU scan in this function first.
          */
-        void scan(int n, int *odata, const int *idata) {
+        void scan(int n, int* odata, const int* idata) {
             timer().startCpuTimer();
             // TODO
+
+            // -------------- Option 1: Single for loop ----------------
+            odata[0] = 0;
+            for (int i = 1; i < n; ++i) {
+                odata[i] = odata[i - 1] + idata[i - 1];
+            }
+
+            // --------------- Option 2: Simulate GPU -------------------
+            //int layer = ilog2ceil(n);
+
+            //// Copy the input to the ping pong buffer
+            //int* ppdata = new int[n];
+            //for (int i = 0; i < n; ++i) {
+            //    ppdata[i] = idata[i];
+            //}
+
+            //// Scan
+            //for (int d = 1; d <= layer; ++d) {
+            //    int offset = 1 << (d - 1);
+            //    for (int i = 0; i < n; ++i) {
+            //        if (i >= offset) {
+            //            odata[i] = ppdata[i - offset] + ppdata[i];
+            //        } else {
+            //            odata[i] = ppdata[i];
+            //        }
+            //    }
+            //    std::swap(odata, ppdata);
+            //}
+            //// Shift right
+            //odata[0] = 0;
+            //for (int i = 1; i < n; ++i) {
+            //    odata[i] = ppdata[i - 1];
+            //}
+            //delete[] ppdata;
+
+            // -----------------------------------------------------------
             timer().endCpuTimer();
         }
 
@@ -31,8 +68,16 @@ namespace StreamCompaction {
         int compactWithoutScan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
             // TODO
+            int idx = 0;
+            for (int i = 0; i < n; ++i) {
+                if (idata[i] != 0) {
+                    odata[idx] = idata[i];
+                    idx++;
+                }
+            }
+
             timer().endCpuTimer();
-            return -1;
+            return idx;
         }
 
         /**
@@ -43,8 +88,38 @@ namespace StreamCompaction {
         int compactWithScan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
             // TODO
+            // Create a boolean array
+            int* boolVal = new int[n];
+            int cnt = 0;
+            for (int i = 0; i < n; ++i) {
+                if (idata[i] == 0) {
+                    boolVal[i] = 0;
+                } else {
+                    boolVal[i] = 1;
+                    cnt++;
+                }
+            }
+
+            // Scan
+            int* scanVal = new int[n];
+            scanVal[0] = 0;
+            for (int i = 1; i < n; ++i) {
+                scanVal[i] = scanVal[i - 1] + boolVal[i - 1];
+            }
+
+            // Scatter
+            for (int i = 0; i < n; ++i) {
+                if (boolVal[i]) {
+                    int idx = scanVal[i];
+                    odata[idx] = idata[i];
+                }
+            }
+
+            delete[] boolVal;
+            delete[] scanVal;
+
             timer().endCpuTimer();
-            return -1;
+            return cnt;
         }
     }
 }
--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -13,12 +13,119 @@ namespace StreamCompaction {
         }
         // TODO: __global__
 
+
+        __global__ void kernNaiveScan(int n, int* odata, const int* idata, int* blockSum) {
+            int id = blockIdx.x * blockDim.x + threadIdx.x;
+            int thid = threadIdx.x;
+            int size = blockDim.x;
+
+            if (id >= n) return;
+
+            // Shared memory array
+            extern __shared__ int temp[];
+
+            // Set ping pong buffer flags
+            int pout = 0, pin = 1;
+
+            // Load array
+            temp[pout * size + thid] = (thid > 0) ? idata[id - 1] : 0;  // thread 0 -> 0, others -> idata[id - 1]
+            __syncthreads();
+
+            // Scan within the block
+            for (int offset = 1; offset < n; offset *= 2) {
+                pout = 1 - pout;
+                pin = 1 - pout;
+                if (thid >= offset) {
+                    temp[pout * size + thid] = temp[pin * size + thid] + temp[pin * size + thid - offset];
+                }
+                else {
+                    temp[pout * size + thid] = temp[pin * size + thid];
+                }
+                __syncthreads();
+            }
+
+            odata[id] = temp[pout * size + thid];
+
+            // Write block sum
+            if (threadIdx.x == blockDim.x - 1 || id == n - 1) {
+                int blockEnd = (n < (blockIdx.x + 1) * blockDim.x) ? n : ((blockIdx.x + 1) * blockDim.x); // last global index in this block
+                blockSum[blockIdx.x] = odata[blockEnd - 1] + idata[blockEnd - 1];
+            }
+        }
+
+        __global__ void kernAddBlockOffset(int n, int* odata, const int* blockOffset) {
+            int id = blockIdx.x * blockDim.x + threadIdx.x;
+            if (id >= n) return;
+            odata[id] += blockOffset[blockIdx.x];
+        }
+
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
             // TODO
+            int threads = 1024;
+            int blocks = (n + threads - 1) / threads; // ceil
+            
+            // Allocate & copy memory
+            int* d_odata, *d_idata;
+            
+            cudaMalloc(&d_odata, n * sizeof(int));
+            checkCUDAError("Naive::cudaMalloc d_odata fails!");
+
+            cudaMalloc(&d_idata, n * sizeof(int));
+            checkCUDAError("Naive::cudaMalloc d_idata fails!");
+            
+            cudaMemcpy(d_idata, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+            checkCUDAError("Naive::cudaMemcpyHostToDevice fails!");
+
+            // Handle array of arbitrary length - create and set blockSum
+            int* blockSum = new int[blocks];
+
+            int* d_blockSum;
+            cudaMalloc(&d_blockSum, blocks * sizeof(int));
+            checkCUDAError("Naive::cudaMalloc d_blockSum fails!");
+
+            cudaMemset(d_blockSum, 0, blocks * sizeof(int)); // Initialize the sum to 0
+            checkCUDAError("Naive::cudaMemset d_blockSum fails!");
+
+            // Call kernNaiveScan
+            kernNaiveScan<<<blocks, threads, 2 * threads * sizeof(int)>>>(n, d_odata, d_idata, d_blockSum);
+            checkCUDAError("Naive::kernNaiveScan fails!");
+
+            // Handle array of arbitrary length - scan the blockSum (on CPU)
+            cudaMemcpy(blockSum, d_blockSum, blocks * sizeof(int), cudaMemcpyDeviceToHost);
+            checkCUDAError("Naive::cudaMemcpyDeviceToHost fails!");
+
+            int* blockOffset = new int[blocks];
+            blockOffset[0] = 0;
+            for (int i = 1; i < blocks; ++i) {
+                blockOffset[i] = blockOffset[i - 1] + blockSum[i - 1];
+            }
+
+            // Handle array of arbitrary length - add the offset back to array
+            int* d_blockOffset;
+            cudaMalloc(&d_blockOffset, blocks * sizeof(int));
+            checkCUDAError("Naive::cudaMalloc d_blockOffset fails!");
+
+            cudaMemcpy(d_blockOffset, blockOffset, blocks * sizeof(int), cudaMemcpyHostToDevice);
+            checkCUDAError("Naive::cudaMemcpyHostToDevice fails!");
+
+            // Call kernAddBlockOffset
+            kernAddBlockOffset<<<blocks, threads>>>(n, d_odata, d_blockOffset);
+
+            // Copy the value back
+            cudaMemcpy(odata, d_odata, n * sizeof(int), cudaMemcpyDeviceToHost);
+            checkCUDAError("Naive::cudaMemcpyDeviceToHost fails!");
+
+            delete[] blockSum;
+            delete[] blockOffset;
+            cudaFree(d_idata);
+            cudaFree(d_odata);
+            cudaFree(d_blockSum);
+            cudaFree(d_blockOffset);
+
             timer().endGpuTimer();
         }
     }
